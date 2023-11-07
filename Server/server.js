132bcf9dcf45e5887d3a793fe001dc375af3833b
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-11-08 01:57:15 +0530

    updated render

--- Server/server.js
+++ Server/server.js
@@ -46,7 +46,7 @@ optionsSuccessStatus: 200,
  credentials: true,
  };
 //app.use(cors(corsOptions));
-app.use(cors({credentials : true, origin: 'http://localhost:5173', }))
+app.use(cors({credentials : true, origin: 'https://reservify-o0b2.onrender.com', }))
 
 //Mongoose Connection
 mongoose.connect(process.env.Mongo_ConnectionString, {
