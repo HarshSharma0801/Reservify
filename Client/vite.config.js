93cd2b01967f971f983bc04da091f593484f2715
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-11-08 02:31:54 +0530

    remove dayjs

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Externalize "dayjs"
-      external: ['dayjs'],
-    },
-  },
+ 
 
 
 
