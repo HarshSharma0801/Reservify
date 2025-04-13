256e67cfb49168306935350cf01a8f1bb84b4a57
HarshSharma0801 <harsh08_sharma@yahoo.com>
2025-04-13 16:19:37 +0530

    done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,7 +12,7 @@ import ReservePage from "./Components/Pages/SingleReserve/ReservePage"
 import Success from "./Components/Pages/SingleReserve/success"
 import YourBooking from "./Components/YourBooking/YourBooking"
 
-axios.defaults.baseURL = 'https://reservify-backend.vercel.app';
+axios.defaults.baseURL = 'https://api.reservify.xyz';
 // axios.defaults.baseURL = 'http://localhost:5005';
 
 axios.defaults.withCredentials = true;
